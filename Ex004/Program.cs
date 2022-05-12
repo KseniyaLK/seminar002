69a35bcbfaa9a9cc18d1e42eb64052b1fc216874
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-12 22:21:06 +0300

    ex004_2

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -4,15 +4,16 @@ Console.WriteLine("введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 
-//if (num % 7 == 0, num % 23 == 0) Console.WriteLine("кратно"); 
-if (num % 7 == 0)
-{
-    if (num % 23 == 0)
-    {
-        Console.WriteLine("кратно");
-    }
+if (num % 7 == 0 && num % 23 == 0) Console.WriteLine("кратно"); 
+//if (num % 7 == 0)
+// {
+//     if (num % 23 == 0)
+//     {
+//         Console.WriteLine("кратно");
+//     }
         
-}
+// }
+
 else Console.WriteLine("не кратно");
 
 

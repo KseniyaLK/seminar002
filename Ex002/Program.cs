63c00e640d96c3080c69521f6090748df8356626
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-12 21:08:05 +0300

    ex002_2

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//напишите программу, которая выводит случайное трехзначное число 
+//и удаляет вторую цифру этого числа
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Number = {number}");
+
+int number1 = number/100;
+Console.WriteLine(number1);
+
+int number2 = number%10;
+// while (number / p > 9) p *= 100;
+// int y = number % p;
+Console.WriteLine(number2);
+Console.WriteLine($"Итог {number1}{number2}");
+

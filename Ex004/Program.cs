e617e6f17b7fdef66dd06d98b61456f5f3129c8e
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-12 21:48:54 +0300

    ex004_1

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,16 +1,19 @@
 ﻿// Напишите программу, которая принимает на вход число и проверяет,
 // кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
 Console.WriteLine("введите число ");
-int num = Convert.ToInt32(Console.ReadLine ());
+int num = Convert.ToInt32(Console.ReadLine());
 
 
- //if (num % 7 == 0, num % 23 == 0) Console.WriteLine("кратно"); 
- if (num%7 == 0)
- {
-     if (num%23 == 0)
-     Console.WriteLine("кратно"); 
- }
- else Console.WriteLine("не кратно");
+//if (num % 7 == 0, num % 23 == 0) Console.WriteLine("кратно"); 
+if (num % 7 == 0)
+{
+    if (num % 23 == 0)
+    {
+        Console.WriteLine("кратно");
+    }
+        
+}
+else Console.WriteLine("не кратно");
 
 
 

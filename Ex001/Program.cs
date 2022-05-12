44c1e2f7eebcb2fe85e6eb4b0be7cd6f2941cd4a
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-12 20:48:53 +0300

    ex001_2

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -7,12 +7,20 @@
 
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Number = {number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-Console.WriteLine($"Number1 = {firstDigit}");
-Console.WriteLine($"Number2 = {secondDigit}");
-Console.WriteLine(firstDigit > secondDigit ? $"Max = {firstDigit}" : $"Max = {secondDigit}");
+int maxNum = MaxNumber (number);
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// Console.WriteLine($"Number1 = {firstDigit}");
+// Console.WriteLine($"Number2 = {secondDigit}");
+//Console.WriteLine(firstDigit > secondDigit ? $"Max = {firstDigit}" : $"Max = {secondDigit}");
 //  if(firstDigit > secondDigit) Console.WriteLine($"Max = {firstDigit}");
 //  else Console.WriteLine($"Max = {secondDigit}");
+Console.WriteLine($"Max = {maxNum}");
 
 
+int MaxNumber (int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}

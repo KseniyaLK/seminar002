1133bb8b7949ee77301f016f46f2311bd0ba7546
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-16 08:55:53 +0300

    ex07

---
+++ Ex007/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+// Примеры:
+// 645 -> 5 ; 78 -> третьей цифры нет; 32679 -> 6
+
+Console.WriteLine("Введите любое число-> ");
+string str = Console.ReadLine();
+
+char[] arr = new char[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    arr[i] = str[i];
+}
+
+if (str.Length > 2) Console.WriteLine($"{str} -> {arr[2]}");
+else Console.WriteLine($"{str} -> третьей цифры нет");
+
+
+
